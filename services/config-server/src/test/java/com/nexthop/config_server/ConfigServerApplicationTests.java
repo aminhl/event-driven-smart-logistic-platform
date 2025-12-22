2f5beaf22d9740e8207601f25ba96fa69ec0372a
aminhl <amin.hlel@esprit.tn>
2025-12-22 20:41:47 +0100

    feat: setup the config server

---
+++ services/config-server/src/test/java/com/nexthop/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexthop.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
